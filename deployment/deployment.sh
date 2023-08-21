17d9cdc4b898055c68ba50b2c58d04e5d0f07b31
Phat Truong <tanphat1991996@gmail.com>
2023-08-21 23:22:58 +0700

    Update deployment.sh

--- deployment/deployment.sh
+++ deployment/deployment.sh
@@ -1,35 +1,12 @@
 #!/bin/bash
 
-# The 3 variables below store server and login details
-# HOST=$1
-# USER=$2
-# PASSWORD=$3
-
-# LOCALFOLDER=$1
-
-# REMOTEFOLDER=$2
-
 FILES="./*"
 
 # Go to local current directory
 cd $LOCAL_FOLDER
-echo $LOCAL_FOLDER
-CWD=$(pwd)
-echo $CWD
-for entry in "$CWD"/*
-do
-  echo "$entry"
-done
-# echo $HOST $USERNAME $PASSWORD
-# FTP login and upload is explained in paragraph below
-ftp -inv $FTP_HOST <<EOF
-user $FTP_USERNAME $FTP_PASSWORD
-
-# Go to local current directory
+ftp -inv <<EOF
+open $FTP_HOST
+user $FTP_USER $FTP_PASSWORD
 cd /test_folder
-CWD=$(pwd)
-echo $CWD
 mput $FILES
-#mdel app_offline.htm
 bye
-EOF
