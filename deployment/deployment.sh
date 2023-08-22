69fcf3495bcf7a6b51fe8db21655b3c3ef315f37
Phat Truong <tanphat1991996@gmail.com>
2023-08-22 13:08:50 +0700

    Update deployment.sh

--- deployment/deployment.sh
+++ deployment/deployment.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
-
-FILES="./*"
-
-# Go to local current directory
-cd $LOCAL_FOLDER
 ftp -inv <<EOF
 open $FTP_HOST
 user $FTP_USERNAME $FTP_PASSWORD
-cd /test_folder
+cd $REMOTE_FOLDER
 prompt
 mput *
 mdel app_offline.htm
